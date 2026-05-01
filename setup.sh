934673e924522f5484e09ab502bdf228e904cb13
Lawrence Liao <lawrence_liao@fas.harvard.edu>
2026-05-01 19:47:15 +0000

    move conda to /root; add auto approve policy

--- setup.sh
+++ setup.sh
@@ -16,8 +16,10 @@ echo 'export CLAUDE_CONFIG_DIR="/workspace/.claude"' >> ~/.bashrc
 echo "Setup conda"
 if [[ ! -f /workspace/miniconda3/bin/conda ]]; then
   wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
-  bash /tmp/miniconda.sh -b -p /workspace/miniconda3
+  bash /tmp/miniconda.sh -b -p /root/miniconda3
   rm /tmp/miniconda.sh
 fi
-/workspace/miniconda3/bin/conda init bash
+/root/miniconda3/bin/conda init bash
+/root/miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/main
+/root/miniconda3/bin/conda tos accept --override-channels --channel https://repo.anaconda.com/pkgs/r
 source ~/.bashrc
